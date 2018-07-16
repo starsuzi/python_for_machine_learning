d61bc31ccfc48c3aa96c5e6d1ce41c0973088e50
정소영 <starsuzi@naver.com>
2018-07-16 17:31:51 +0900

    Add files via upload

---
+++ overview.py
@@ -0,0 +1,11 @@
+#여러 단어들을 하나로 붙일 때
+colors = ['red', 'blue', 'green', 'yellow']
+result = ''
+for s in colors:
+    result += s
+print(result)
+
+#여러 단어들을 하나로 붙일 때
+#colors = ['red', 'blue'. 'green', 'yellow']
+result = ''.join(colors)
+print(result)
---
+++ split&join.py
@@ -0,0 +1,16 @@
+#빈칸을 기준으로 문자열 나누기
+items = 'zero one two three'.split()
+print(items)
+#","을 기준으로 문자열 나누기
+items1 = 'hello,world,hello,python'.split(',')
+print(items1)
+#리스트의 각 값을 a,b,c,변수로 unpacking
+items2 = 'python,jquery,javascript'
+a,b,c = items2.split(",")
+print("this is a: "+a)
+print("this is b: "+b)
+print("this is c: "+c)
+#"."을 기준으로 문자열 나누고 unpacking
+items3 = 'hello.everyone.bye'
+d,e,f = items3.split(".")
+print(d)
